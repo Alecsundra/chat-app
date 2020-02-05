6e1b3588656d66b42185266e065be77d25e67b00
Sandra <alecsandra.serediuc@gmail.com>
2020-02-05 12:12:35 +0200

    Added click button that changes the status

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import './components/Contact.js'
 import Contact from './components/Contact.js';
+// import ContactList from './components/ContactList'
 
+// let list= [
+//   {name: "Sandra Davis", 
+//   avatar: "https://avatars2.githubusercontent.com/u/59797566?s=400&v=4",
+//   online: true },
+//   {name: "Ilona Dany",
+//   avatar: "https://avatars2.githubusercontent.com/u/59797566?s=400&v=4",
+//   online: true},
+//   {name: "John Boy",
+//   avatar: "https://avatars2.githubusercontent.com/u/59797566?s=400&v=4",
+//   online: false},
+//   {name: "Iza Dan",
+//   avatar: "https://avatars2.githubusercontent.com/u/59797566?s=400&v=4",
+//   online: true},
+//   {name: "Ilona Dany",
+//   avatar: "https://avatars2.githubusercontent.com/u/59797566?s=400&v=4",
+//   online: false},
+//   ];
 
-function App() {
+function App(props) {
   return (
     <div>
       <Contact />
+      {/* <ContactList list={list} /> */}
     </div>
   );
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,50 +1,68 @@
 import React, { Component } from 'react'
 import './Contact.css';
 
-let arr= [
-{name: "Sandra Davis", 
-avatar: "https://avatars2.githubusercontent.com/u/59797566?s=400&v=4",
-online: true },
-{name: "Ilona Dany",
-avatar: "https://avatars2.githubusercontent.com/u/59797566?s=400&v=4",
-online: true},
-{name: "John Boy",
-avatar: "https://avatars2.githubusercontent.com/u/59797566?s=400&v=4",
-online: false}
-];
-function Contact() {
+
+class Contact extends Component {
+    constructor(){
+      super()
+      this.state = {
+        list:[
+          {name: "Sandra Davis", 
+          avatar: "https://avatars2.githubusercontent.com/u/59797566?s=400&v=4",
+          online: true },
+          {name: "Ilona Dany",
+          avatar: "https://avatars2.githubusercontent.com/u/59797566?s=400&v=4",
+          online: true},
+          {name: "John Boy",
+          avatar: "https://avatars2.githubusercontent.com/u/59797566?s=400&v=4",
+          online: false}
+          ]
+      };
+    }
+  
+    handleClick= (e) => {
+      let list1=[...this.state.list];
+      list1[e.target.id].online = !list1[e.target.id].online
+      this.setState ({
+        list: list1
+      })
+    };
+      
+  render() {
   return (
   <div>
-  {
-    arr.map( person =>{
+  {this.state.list.map( (person, i) =>{
           return (
-        <div className="Contact">
+        <div className="Contact" key={i}>
           
           <img className="avatar" src={person.avatar}/>
             <div>
              <h4 className="name">{person.name}</h4>
              
-          <div className='status'>
-           {person.online ? <div>
-            <div className="status-online"> </div>
-              <span className='status-text'>Online</span>     
-           </div>   
+          <div className='status' onClick={this.handleClick} >
+           {person.online
+            ? <div>
+                <div className="status-online"></div>
+                <button ><span id={i} className='status-text'>Online</span></button>     
+              </div>   
             :
-            <div><div className="status-offline"></div>
-              <span className='status-text'>Offline</span>
-            </div>
+              <div>
+                <div className="status-offline"></div>
+                <button ><span id={i} className='status-text'>Offline</span></button>
+              </div>
           }
         
         </div>
         </div>
-         </div>
+        </div>
           )
         
       })
     }
-  
+   
       </div>
   )
-};
+}
+}
 
 export default Contact
